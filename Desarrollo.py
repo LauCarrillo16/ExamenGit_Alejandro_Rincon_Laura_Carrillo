a63976222db3acb161b77a6ab18cfe66ff4701cf
AlejandroRinconPerez <alejandro.ingenieria@gmail.com>
2024-07-31 11:41:59 -0500

    feat: :sparkles: New fuction to see citys
    this new fuction will allow the user to check all the cities listed at the Json
    
    @AlejandroRincopn

--- Desarrollo.py
+++ Desarrollo.py
@@ -2,7 +2,6 @@ import json
 
 Ciudades = {}
 
-
 Ruta_JSON_Ciudades = "Ciudades.json"
 
 
@@ -22,28 +21,76 @@ def cargar_datos(Ruta_JSON, Datos):
     except Exception as e:
         print(f"Error al cargar Datos {e}")
 
+
+
 def Registro_dict():
-    cargar_datos(Ruta_JSON_Ciudades, Ciudades)
-    print("""
-            ***********************************
-                Registro de Ciudad Nueva
-            ***********************************
-            """)
-    Codigo_postal = input("Ingrese el codigo postal:  ")
-    if Ciudades.get(Codigo_postal,None)== None:
-        Ciudades[Codigo_postal]={}
-        info_ciudad = {
-            "Nombre":input("Ingrese el Nombre de la Ciudad"),
-            "Poblacion":input("Ingrese la poblacion estimada"),
-            "Pais":input("Ingrese el Nombre del PAIS")
-        }
-        Ciudades[Codigo_postal] = info_ciudad
-        guardar_datos(Ruta_JSON_Ciudades, Ciudades)
+    while True:
+        try:
+            cargar_datos(Ruta_JSON_Ciudades, Ciudades)
+            print("""
+                    ***********************************
+                        Registro de Ciudad Nueva
+                    ***********************************
+                    """)
+            Codigo_postal = input("Ingrese el codigo postal:  ")
+            if Ciudades.get(Codigo_postal,None)== None:
+                Ciudades[Codigo_postal]={}
+                info_ciudad = {
+                    "Nombre":input("Ingrese el Nombre de la Ciudad"),
+                    "Poblacion": int(input("Ingrese el Nueva Poblacion")),
+                    "Pais":input("Ingrese el Nombre del PAIS")
+                }
+                Ciudades[Codigo_postal] = info_ciudad
+                guardar_datos(Ruta_JSON_Ciudades, Ciudades)
+                return
+            else:
+                print("Codigo Registrado Anteriormente")
+        except Exception as e:
+            print(f"Error Causado por {e}")
 
 
-    else:
-        print("Codigo Registrado Anteriormente")
+
+def Modificar_Datos():
+    while True:
+        try:
+            cargar_datos(Ruta_JSON_Ciudades, Ciudades)
+            print("""
+                    ***********************************
+                        Modificar Datos de la ciudad
+                    ***********************************
+                    """)
+            
+            Codigo_postal = input("Ingrese el codigo postal:  ")
+            if Ciudades.get(Codigo_postal,None)!= None:
+                pais = input("Ingrese Pais a Corregir")
+                ciudad = input("Ingrese Nombre de Ciudad nuevo")
+                Ciudades[Codigo_postal]["Nombre"] = ciudad
+                Ciudades[Codigo_postal]["Poblacion"] = int(input("Ingrese el Nueva Poblacion"))
+                Ciudades[Codigo_postal]["Pais"] = pais
+                guardar_datos(Ruta_JSON_Ciudades, Ciudades)
+                return
+        except Exception as e:
+                    print(f"Error Causado por {e}")
+
+def Ver_Todas_las_Ciudades():
+    while True:
+        try:
+            cargar_datos(Ruta_JSON_Ciudades, Ciudades)
+            print("""
+                    ***********************************
+                        Ver Todas las Ciudades
+                    ***********************************
+                    """)
+            for llave, valor in Ciudades.items():
+                print(f"""
+                        Ciudad: {valor.get("Nombre")}
+
+                        Codigo Postal: {llave}
+                        Pais: {valor.get("Pais")}
+                        Poblacion: {valor.get("Poblacion")}
+                        """)
+            return
+        except Exception as e:
+                    print(f"Error Causado por {e}")
 
 
-
-Registro_dict()
